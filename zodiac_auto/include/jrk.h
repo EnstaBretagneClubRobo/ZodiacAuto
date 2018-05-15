1c719986aad0ad88866ebe282ec3879f08265d73
Mael <legal.mael@hotmail.fr>
2018-05-15 08:45:50 +0200

    add pololu jrk driver

---
+++ zodiac_auto/include/jrk.h
@@ -0,0 +1,7 @@
+int jrkConnect(const char * device);
+int jrkGetVariable(int fd, unsigned char command);
+int jrkGetFeedback(int fd);
+int jrkGetTarget(int fd);
+int jrkSetTarget(int fd, unsigned short target);
+void jrkTest(int fd);
+
---
+++ zodiac_auto/src/motorCtr.cpp
@@ -0,0 +1,46 @@
+/*------------------------------------------------------------------------------
+* ROS node "motorCtr" subscribes to "cmd" topic which publishes Int32 command
+* messages.
+* It connects to the Pololu Jrk and sends new targets depending on the
+* command received.
+* ------------------------------------------------------------------------------
+*/
+
+#include <ros/ros.h>
+#include <string.h>
+#include "jrk.h"
+#include "std_msgs/Int32.h"
+
+using namespace std;
+
+int fd;
+
+void cmd_callback(const std_msgs::Int32::ConstPtr& msg)
+{
+	// For now, the command is directly sent to the Pololu.
+    jrkSetTarget(fd, msg->data);
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "motorCtr");
+    ros::NodeHandle nh;
+    ros::Subscriber cmd_sub = nh.subscribe("cmd", 1000, cmd_callback);
+
+    string path;
+    nh.param<string>("path", path, "/dev/ttyACM0");
+
+    // ROS_INFO(path.c_str());
+    if ((fd = jrkConnect(path.c_str()) ) != -1)
+    {
+        ROS_INFO("Jrk connected");
+
+        ROS_INFO("Jrk test");
+        jrkTest(fd);
+
+        ros::spin();
+        close(fd);
+    }
+
+    return 0;
+}
