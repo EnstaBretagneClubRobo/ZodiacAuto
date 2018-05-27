8bb349d4535a652be3c47b16650bb6f4c0d3bd61
Joris T <joris.tillet@ensta-bretagne.org>
2018-05-27 18:19:04 +0200

    remove useless comments

--- echosounder/src/depth_publisher.py
+++ echosounder/src/depth_publisher.py
@@ -7,7 +7,7 @@ from std_msgs.msg import Float64
 rospy.init_node("depth_publisher")
 depth_pub = rospy.Publisher("depth", Float64, queue_size=1)
 
-ser = serial.Serial(port="/dev/sonar", baudrate=9600) #, parity=serial.PARITY_ODD, stopbits=serial.STOPBITS_TWO, bytesize=serial.SEVENBITS)
+ser = serial.Serial(port="/dev/sonar", baudrate=9600)
 
 rate = rospy.Rate(5)
 if ser.isOpen():
@@ -15,7 +15,7 @@ if ser.isOpen():
         line = ser.readline()
         if line[0] == '0':
             depth_msg = Float64()
-            depth_msg.data = float(line.split('m')[0]) #TODO To be changed
+            depth_msg.data = float(line.split('m')[0])
             depth_pub.publish(depth_msg)
         rate.sleep()
     ser.close()
